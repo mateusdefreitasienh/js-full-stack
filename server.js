6f1b671393cdd2e298918af2e5b6170889b4fa6d
Mateus de Freitas <mateusdefreitas@ienh.com.br>
2026-06-01 21:43:31 -0300

    criacao endpoint personagens

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ import express from "express";
 const port = 8080;
 const app = express();
 
+app.use(express.json());
+
 app.get('/mateus', (req, res) => {
     res.send('<h1>Mateus</h1>')
 });
@@ -10,5 +12,15 @@ app.get('/mateus', (req, res) => {
 app.get('/teste', (req, res) => {
     res.send('<h1>Teste</h1><img src="https://aquitemplacas.s3.sa-east-1.amazonaws.com/produtos/3874-controle-de-qualidade-testes.jpg">')
 });
+
+app.get('/personagens', (req, res) => {
+    const personagens = [
+        { nome: 'Super Choque', idade: 14 },
+        { nome: 'Goku', idade: 50 },
+        { nome: 'Vegeta', idade: 60 },
+    ];
+
+    res.json(personagens)
+})
 
 app.listen(port, () => console.log("O server está on!"));
